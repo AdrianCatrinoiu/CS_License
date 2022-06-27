0a3b91ed04ad60fd720d93a3b9bf40a83ef7995e
AdrianCatrinoiu <ady.catrinoiu@gmail.com>
2022-06-27 17:42:54 +0300

    tab icon+name

--- src/index.js
+++ src/index.js
@@ -8,10 +8,15 @@ import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import "./input.css";
 import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
+import { Helmet } from "react-helmet";
 
 Kommunicate.init("12fe8796000bf5a4f00d46fe7e7d545ff");
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Nortec Noir</title>
+    </Helmet>
     <Provider store={store}>
       <BrowserRouter>
         <PersistGate persistor={persistor}>
